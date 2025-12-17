0f3452f5b5acd27c61890f59ba4c4f80e4b9ab26
Prahlad <prahlady0001@gmail.com>
2025-12-18 00:16:49 +0530

    Push to Remote

---
+++ src/main/java/com/spring_jenkins/controller/JenController.java
@@ -0,0 +1,16 @@
+package com.spring_jenkins.controller;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/jen")
+public class JenController {
+
+    @GetMapping
+    public String print() {
+        return "Wellcome";
+    }
+}
